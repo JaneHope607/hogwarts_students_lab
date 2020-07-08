40e2df8968827002b2511c663d5ebde7d2baa226
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-08 17:58:47 +0100

    created find methods for house class

--- models/house.rb
+++ models/house.rb
@@ -25,4 +25,18 @@ class House
         SqlRunner.run(sql)
     end
 
+    def self.find_all()
+        sql = "SELECT * FROM houses"
+        house_data = SqlRunner.run(sql)
+        return house_data.map{ |house| House.new(house) }
+    end
+
+    def find_by_id(id)
+        sql = "SELECT * FROM
+        houses WHERE ID = $1"
+        values = [@id]
+        house_data = SqlRunner.run(sql, values).first
+        return House.new(house_data)
+    end
+
 end
